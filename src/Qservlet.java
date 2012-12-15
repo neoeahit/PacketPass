6135efa24b03a7079c5e43614cec0b253644ab03
Vipul <vipul@ubuntu.ubuntu-domain>
2012-12-15 16:49:44 -0500

    First commit

---
+++ src/Qservlet.java
@@ -0,0 +1,107 @@
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Qservlet
+ */
+public class Qservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Qservlet()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			processRequest(request, response);
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			processRequest(request, response);
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+		//Get parameters from calling servlet
+		String vmname=request.getParameter("vmname");
+		String zone=request.getParameter("zone");
+		
+		// response.setContentType("text/html;charset=UTF-8");
+		//PrintWriter out = response.getWriter();
+		
+		//out.println("<html><h3>VM "+vmname+" will be moved to "+zone+"</h3></html>");
+		
+		//instead we are writing it to the file and nodejs will take of it
+		try {
+			 
+			String content = vmname+" "+zone;
+ 
+			File file = new File("/home/vipul/Downloads/PacketPass/src/nodejsfile.txt");
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Put a wait till the operations are complete (Do not use Thread.sleep, find alternate way)
+		//Thread.sleep(10000);
+		
+		//Call the homepage servlet again
+		//response.sendRedirect("http://localhost:9080/PacketPass/homepage");
+	}
+
+}
---
+++ src/homepage.java
@@ -0,0 +1,211 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class homepage
+ */
+public class homepage extends HttpServlet
+{
+	public class SecGroup
+	{
+		String name;
+		ArrayList<VM> vmlist = new ArrayList<VM>();
+	}
+	
+	public class VM
+	{
+		String name;
+		String ip;
+	}
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public homepage()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @throws Exception 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception 
+    {
+		// TODO Auto-generated method stub
+    	response.setContentType("text/html;charset=UTF-8");
+    	
+    	ArrayList<SecGroup> grouplist = new ArrayList<SecGroup>();
+    	
+    	File file = new File("/home/vipul/Downloads/PacketPass/src/config.txt");
+    	Scanner scanner = new Scanner(file);
+        
+    	int numvms = 0;
+    	int numgroups = 0;
+    	
+    	while ( scanner.hasNextLine() )
+        {
+        	String line = scanner.nextLine();
+            if( line.toLowerCase().contains("group") )
+            {
+            	//add new group to list
+            	SecGroup g = new SecGroup();
+            	g.name = line.trim();
+            	
+            	line = scanner.nextLine();
+            	
+            	//add vm's to last group
+            	StringTokenizer stk = new StringTokenizer(line, ";");
+            	while(stk.hasMoreTokens())
+            	{
+            		String token = stk.nextToken().trim();
+            		VM tempvm = new VM();
+            		tempvm.ip = token;
+            		tempvm.name = token;
+            		g.vmlist.add(tempvm);
+            		numvms++;
+            	}
+            	
+            	grouplist.add(g);
+            	numgroups++;
+            }
+        }
+    	
+    	PrintWriter out = response.getWriter();
+    	
+    	/*Output HTML/Javascript page*/
+    	out.println("<!doctype html>" + 
+    				"\n<html lang=\"en\">" + 
+    				"\n<head>" +
+				    "\n<meta charset=\"utf-8\" />" +
+				    "\n<title>Packetpass Firewall Manager</title>" +
+				    "\n<link rel=\"stylesheet\" href=\"http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css\" />" +
+				    "\n<script src=\"http://code.jquery.com/jquery-1.8.2.js\"></script>" +
+				    "\n<script src=\"http://code.jquery.com/ui/1.9.1/jquery-ui.js\"></script>" +
+				    "\n<link rel=\"stylesheet\" href=\"/resources/demos/style.css\" />" +
+				    "\n<style>");
+    	
+    	for(int i=1;i<=numgroups;i++)
+    	{
+    		out.println("\n#droppable" +i+" { width: 200px; height: 100px; padding: 0.5em; float: left; margin: 20px; }");
+    	}
+    	for(int i=1;i<=numvms;i++)
+    	{
+    		out.println("\n#draggable"+i+" { width: 30px; height: 50px; padding: 0.5em; float: left; margin: 20px 20px 20px 0; }");
+    	}
+    	out.println("\n</style>");
+    	out.println("\n<script>");
+    	
+    	out.println("\nvar hidden = null;");
+    	
+    	out.println("\nfunction placement(zone)" +
+    			"\n{" +
+    			"\nalert(hidden + \" moved to \" + zone);" +
+    			"\nloadSQS(hidden, zone);" + 
+    			"\n}" +
+    			"\nfunction setHidden(hiddenvalue)" +
+    			"\n{" +
+    				"\nhidden = hiddenvalue;" +
+    				"\nreturn true;" +
+    			"\n}");
+    	
+    	out.println("function loadSQS(vm, zone)" + 
+    				"{" +  
+    				"document.location.href = \"Qservlet?vmname=\"+vm+\"&zone=\"+zone;" +  
+    				"}");
+    	
+    	out.println("\n$(function() {");
+    	
+    	for(int i=1;i<=numvms;i++)
+    	{
+    		out.println("\n$( \"#draggable"+i+"\" ).draggable();");
+    	}
+    	int i = 1;
+    	for(SecGroup group:grouplist)
+    	{
+    		out.println("\n$( \"#droppable"+i+"\" ).droppable({" + 
+    					"\ndrop: function( event, ui ) {" + 
+    					"\nplacement(\""+group.name+"\");" + 
+    					"\n}" + 
+    					"\n});");
+    		i++;
+    	}
+    	
+    	out.println("\n});");
+    	
+    	out.println("\n</script>" + 
+    				"\n</head>" + 
+    				"\n<body>");
+    	
+    	i =1;
+    	int j=1;
+    	for(SecGroup group:grouplist)
+    	{
+    		//display group
+    		
+    		out.println("\n<div id=\"droppable"+j+"\" class=\"ui-widget-header\">" + 
+    					"\n<p>" + group.name + "</p>" + 
+    					"\n</div>");
+    		
+    		for(VM vmac:group.vmlist)
+    		{
+    			//display VM
+    			out.println("\n<div id=\"draggable"+i+"\" class=\"ui-widget-content\" onMouseDown=\"setHidden('"+vmac.name+"');\">" + 
+    						"\n<p>"+vmac.name+"</p>" + 
+    						"\n</div>");
+    			i++;
+    		}
+    		
+    		out.println("\n<br><br><br><br><br><br><br><br><br>");
+    		j++;
+    	}
+    	
+    	out.println("\n</body></html>");
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+		// TODO Auto-generated method stub
+		try
+		{
+			processRequest(request, response);
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			processRequest(request, response);
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
