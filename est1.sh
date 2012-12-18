9b2871afc437f26597ba9231e86e545b2c4c1a23
Vipul <neoeahit@gmail.com>
2012-12-18 01:26:26 -0500

    adding for http and https

--- est1.sh
+++ est1.sh
@@ -21,10 +21,28 @@ if [ $ssh = $t ]
   then
     for ((i=5; i<${#Array[@]}; ++i));
       do
-        iptables -A INPUT -p tcp -s ${Array[i]} --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT   
+        iptables -A INPUT -p tcp -s ${Array[i]} --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+        iptables -A OUTPUT -p tcp -d ${Array[i]} --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT   
     done
 fi
-
+#for http
+if [ $http = $t ]
+  then
+    for ((i=5; i<${#Array[@]}; ++i));
+      do
+        iptables -A INPUT -p tcp --sport 1024:65535 -s ${Array[i]} --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -d ${Array[i]} --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+    done
+fi
+#for https
+if [ $https = $t ]
+  then
+    for ((i=5; i<${#Array[@]}; ++i));
+      do
+      iptables -A INPUT -p tcp -s  ${Array[i]} --sport 1024:65535 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+      iptables -A OUTPUT -p tcp --sport 443 -d  ${Array[i]} --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT
+     done
+fi
 iptables -P INPUT DROP
 iptables -L -v
 service iptables save
